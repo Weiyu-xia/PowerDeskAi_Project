483448c463bbe57d84a1ed456be738c3ee85b4f3
Weiyu-xia <124910304+Weiyu-xia@users.noreply.github.com>
2024-08-19 14:34:47 +0800

    Users can type into html pages and get the output

--- Dawatt/views.py
+++ Dawatt/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
-
 from Dawatt.client import Call_Dawatt
 
 
 class DawattView(TemplateView):
     template_name = 'Dawatt/welcome.html'
 
-    extra_context = {
-        'reply': Call_Dawatt("介绍一下南方电网"),
+    # 从表单获取用户的输入
+    def post(self, request, *args, **kwargs):
+        user_input = request.POST.get('user_input', '')
+        reply = Call_Dawatt(user_input)
+        context = self.get_context_data(reply=reply, user_input=user_input)
+        return self.render_to_response(context)
 
-    }
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
