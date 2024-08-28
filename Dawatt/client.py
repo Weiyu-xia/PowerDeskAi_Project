44257ea355aafa218bd17705dc4437c9664661d4
Weiyu-xia <124910304+Weiyu-xia@users.noreply.github.com>
2024-08-28 15:26:48 +0800

    fix the function name bug

--- Dawatt/client.py
+++ Dawatt/client.py
@@ -2,7 +2,7 @@ import re
 import requests
 
 
-def Call_Dawatt_history(chat_history):
+def Call_Dawatt(chat_history):
     url = "http://192.168.210.128:1060/v1/chat/completions"
     params = {
         "model": "Megawatt_13b",
@@ -27,11 +27,11 @@ def Call_Dawatt_history(chat_history):
     return words
 
 
-
 def generate_sql(query):
     # 调用大模型生成SQL查询语句
     chat_history = [{"role": "system", "content": "请生成一个对应的SQL查询语句：" + query}]
-    return Call_Dawatt_history(chat_history)
+    return Call_Dawatt(chat_history)
+
 
 def Print_Dawatt(chat_history):
     # 将聊天记录转换为格式化字符串
