17c61ce063da07a2729ed422be5f5d149b738211
MattChen2002 <chenzixin202209@163.com>
2025-01-20 13:49:46 +0800

    upload the emotion analysis function and the next questions suggestions generated funtion.

--- PowerDeskAi_Project/settings.py
+++ PowerDeskAi_Project/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,9 +23,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-gca(4^e=wr^_&xx1gm--o8+0wv2_pz(8e*tmzlduv)e^++i*b+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+# 允许的服务器
+ALLOWED_HOSTS = ['*']
+
+# 静态文件收集目录
+STATIC_ROOT = os.path.join(BASE_DIR, 'collected_static')
 
 # 使用Django内置登录方法
 LOGIN_URL = 'login'
